19e1855418dab83d0d6d5538c7a9a76dd1c51372
choeminjun <choeminjun@naver.com>
2018-05-05 15:10:58 +0900

    creating views, templates

--- models/boxes/views.py
+++ models/boxes/views.py
@@ -4,7 +4,7 @@ from flask import Blueprint, request, session, url_for, render_template, flash
 from werkzeug.utils import redirect
 from models.notes.note import Note
 import models.users.decorators as user_decorators
-from models.users.user import User
+from models.notes.note import Note
 from models.error_logs.error_log import Error_
 import traceback
 from config import ELASTIC_PORT as port
@@ -14,4 +14,20 @@ box_blueprint = Blueprint('boxs', __name__)
 
 @box_blueprint.route('/box/<string:box_id>')
 def box(box_id):
+    pass
+
+
+@box_blueprint.route('/box/create', methods=['GET', 'POST'])
+@user_decorators.require_login
+def create_box():
+    all_notes = Note.get_user_notes(session['email'])
+    if request.mehtod == 'POST':
+        pass
+
+    return render_template('boxs/create_box.html', all_notes=all_notes)
+
+
+@box_blueprint.route('/box/delete')
+@user_decorators.require_login
+def delete_box():
     pass
