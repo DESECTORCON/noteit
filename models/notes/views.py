9902fe972a0d71484482eeff95c8812e049a9cf9
choeminjun <choeminjun@naver.com>
2018-05-13 15:11:42 +0900

    Final commit

--- models/notes/views.py
+++ models/notes/views.py
@@ -225,7 +225,7 @@ def create_note():
                 return redirect(url_for(".user_notes"))
             
             # saving note
-            all_box_id = Box.find_by_id(User.find_by_id(session['_id']).All_box_id)
+            all_box_id = Box.find_by_id(User.find_by_id(session['_id']).All_box_id)._id
             note_for_save = Note(title=title, content=content, author_email=author_email, shared=share,
                                  author_nickname=author_nickname, share_only_with_users=share_only_with_users,
                                  share_label=label, file_name=filenames, box_id=all_box_id)
