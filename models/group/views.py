ae326930665f15f6c073cd0303d7381d1196e9b4
choeminjun <choeminjun@naver.com>
2018-05-20 14:23:11 +0900

    Making template for create_group

--- models/group/views.py
+++ models/group/views.py
@@ -1,7 +1,10 @@
 import os
-
-from flask import Blueprint, request, render_template, session
+import traceback
+import shortid
+from flask import Blueprint, request, render_template, session, url_for
+from werkzeug.utils import secure_filename, redirect
 import models.users.decorators as user_decorators
+from models.error_logs.error_log import Error_
 from models.group.group import Group
 from models.users.user import User
 from models.group.constants import ALLOWED_GROUP_IMG_FORMATS
@@ -9,39 +12,81 @@ from models.group.constants import ALLOWED_GROUP_IMG_FORMATS
 group_blueprint = Blueprint('groups', __name__)
 
 
+def share_bool_function(share):
+    if share == '0':
+        share = False
+    elif share == '1':
+        share = True
+
+    return share
+
+
 @group_blueprint.route('/groups', methods=['GET', 'POST'])
 @user_decorators.require_login
 def groups():
-    all_groups = Group.get_all_shared_groups()
-    
-    if request.method is 'POST':
-        form = request.form['Search']
-        all_groups = Group.search_with_elastic(form_data=form, shared=True)
-    
-    return render_template('groups/shared_groups.html', all_groups=all_groups)
+    try:
+        all_groups = Group.get_all_shared_groups()
+
+        if request.method is 'POST':
+            form = request.form['Search']
+            all_groups = Group.search_with_elastic(form_data=form, shared=True)
+
+        return render_template('groups/shared_groups.html', all_groups=all_groups)
+    except:
+        error_msg = traceback.format_exc().split('\n')
+
+        Error_obj = Error_(error_msg=''.join(error_msg), error_location='groups shared_groups')
+        Error_obj.save_to_mongo()
+        return render_template('error_page.html', error_msgr='Crashed during getting groups...')
 
 
 @group_blueprint.route('/create_group', methods=['GET', 'POST'])
 @user_decorators.require_login
 def create_group():
-    all_firends = []
-    current_user = User.find_by_id(session['_id'])
-    for friend_id in current_user.friends:
-        all_firends.append(User.find_by_id(friend_id))
+    try:
+        all_firends = []
+        current_user = User.find_by_id(session['_id'])
+        for friend_id in current_user.friends:
+            all_firends.append(User.find_by_id(friend_id))
 
-    if request.method is 'POST':
-        name = request.form['name']
-        members = request.form.getlist('members')
-        group_img = request.form['img']
-        file_name, file_extenstion = os.path.splitext(group_img)
-        if file_extenstion not in ALLOWED_GROUP_IMG_FORMATS or len(group_img) > 1:
-            return render_template('groups/create_group.html',
-                                   all_firends=all_firends, error_msg='Too much images!! Please upload just one image.',
-                                   name=name, members=members)
+        if request.method is 'POST':
+            name = request.form['name']
+            members = request.form.getlist('members')
+            group_img = request.form['img']
+            if group_img is not None:
+                file_name, file_extenstion = os.path.splitext(group_img)
+                if file_extenstion not in ALLOWED_GROUP_IMG_FORMATS or len(group_img) > 1:
+                    return render_template('groups/create_group.html',
+                                           all_firends=all_firends, error_msg='Too much images!! Please upload just one image.',
+                                           name=name, members=members)
 
-        shared = request.form.get("shared") != None
+                # saving file
+                # create name for file
+                sid = shortid.ShortId()
+                # create path for file
+                file_path, file_extenstion = os.path.splitext(group_img.filename)
+                filename = secure_filename(sid.generate()) + file_extenstion
 
+                # os.chdir("static/img/file/")
+                # save file and add file to filenames list
+                group_img.save(os.path.join(filename))
+            else:
+                filename = None
 
+            shared = request.form.get("shared") is not None
 
+            # saving group
+            group_for_save = Group(name=name, members=members, group_img_name=filename, shared=shared, shared_notes=[])
+            group_for_save.save_to_mongo()
+            group_for_save.save_to_elastic()
+            # redirecting
 
-    return render_template('groups/create_group.html', all_firends=all_firends)
+            return redirect(url_for('.home'))
+
+        return render_template('groups/create_group.html', all_firends=all_firends)
+    except:
+        error_msg = traceback.format_exc().split('\n')
+
+        Error_obj = Error_(error_msg=''.join(error_msg), error_location='create_group creating group')
+        Error_obj.save_to_mongo()
+        return render_template('error_page.html', error_msgr='Crashed during creating your group...')
