e2ed0016c66eb78fd1d172d1557d99fe61353965
choeminjun <choeminjun@naver.com>
2018-06-13 11:55:00 +0900

    Building new model - notifications --created delete page

--- models/notification/views.py
+++ models/notification/views.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, url_for
+from werkzeug.utils import redirect
 from models.notification.notification import Notification
 
 notification_blueprint = Blueprint('notification', __name__)
@@ -9,3 +10,9 @@ def notification(notification_id):
     notifi = Notification.find_by_id(notification_id)
     
     return render_template('notifications/notification.html', notifi=notifi)
+
+
+@notification_blueprint.route('/notifi/delete/<string:notifi_id>')
+def delete_notifi(notifi_id, redirect_to):
+    Notification.find_by_id(notifi_id).delete()
+    return redirect(url_for(redirect_to))
