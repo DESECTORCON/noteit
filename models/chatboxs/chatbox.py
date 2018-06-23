2c6f660a2398308f7940ee05fb816a557e2de229
choeminjun <choeminjun@naver.com>
2018-06-23 15:56:05 +0900

    Making chatbox model for messages -- making modules

--- models/chatboxs/chatbox.py
+++ models/chatboxs/chatbox.py
@@ -1,6 +1,7 @@
 import datetime
 import uuid
 from common.database import Database
+from models.chatboxs.constants import COLLECTION as ChatBoxConstants
 
 
 class ChatBox(object):
@@ -22,4 +23,24 @@ class ChatBox(object):
         }
 
     def save_to_mongo(self):
-        Database.insert(ErrorLogConstants.COLLECTION, self.json())
+        Database.insert(ChatBoxConstants, self.json())
+
+    @classmethod
+    def find_by_id(cls, chatbox_id):
+        try:
+            return cls(**Database.find_one(ChatBoxConstants.COLLECTION, {'_id': chatbox_id}))
+        except TypeError:
+            return None
+
+    def delete_member(self, member_id):
+        # returns false when error accorded
+        try:
+            self.user_ids.remove(member_id)
+        except ValueError:
+            return False
+
+    def add_member(self, user_id):
+        # addes member by extending list
+        self.user_ids.extend([user_id])
+
+
