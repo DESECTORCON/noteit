fcb490488d7bb5ec9adfc12122b10f35efa82dc9
choeminjun <choeminjun@naver.com>
2018-08-05 12:37:17 +0900

    Cert keys added

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
 from app import app
 
-app.run(debug=app.config['DEBUG'], port=4990,  threaded=True)
+app.run(debug=app.config['DEBUG'], port=4990,  threaded=True
+        , ssl_context=('/Users/choeminjun/src/noteit/cert/server.crt', '/Users/choeminjun/src/noteit/cert/server.key'))
