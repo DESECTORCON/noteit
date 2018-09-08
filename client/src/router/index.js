28963faad5653da78a7dcdd94061b05e856a09b5
choeminjun <choeminjun@naver.com>
2018-09-08 14:32:38 +0900

    learning flask-vue --3

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Ping from '@/components/Ping';
+import Books from '@/components/Books';
 
 Vue.use(Router);
 
@@ -8,8 +9,14 @@ export default new Router({
   routes: [
     {
       path: '/',
+      name: 'Books',
+      component: Books,
+    },
+    {
+      path: '/ping',
       name: 'Ping',
       component: Ping,
     },
   ],
+  mode: 'hash',
 });
